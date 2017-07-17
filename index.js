e083fc7b423c4d51816012b6bfc19c7170c1ad34
David Isovitsch <isovitd@nationwide.com>
2017-07-17 17:31:08 -0400

    Simply app.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 const express = require('express')
 const app = express()
 
@@ -6,3 +7,19 @@ app.use(express.static('app'))
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
+*/
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
